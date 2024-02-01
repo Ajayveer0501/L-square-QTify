abfb351103d2cc9d0690924c4a7022ef62e1d74a
Ajay Veer Singh <avisingh.veer@gmail.com>
2024-02-02 01:41:05 +0530

    ME_QTIFY_MODULE_NAVBAR

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LogoImage from "../../assets/Main_logo.png"
+
+const Logo = () => {
+  return (
+    <img src={LogoImage} alt="Logo" width={67} />
+  )
+}
+
+export default Logo
